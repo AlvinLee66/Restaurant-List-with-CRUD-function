130f2cfd40b736833886b8dd4ea1435f71d34271
AlvinLee66 <ewqaz001@gmail.com>
2022-04-16 16:46:49 +0800

    move route search to home

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,8 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
-const search = require('./modules/search')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
-router.use('/search', search)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -13,6 +13,25 @@ router.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase().trim()
+
+  if (!keyword.length) {
+    return res.redirect('/')
+  }
+
+  Restaurant.find()
+    .lean()
+    .then(restaurantData => {
+      const restaurants = restaurantData.filter(restaurant => {
+        const combineNameAndCategory = restaurant.name + restaurant.category
+        return combineNameAndCategory.toLowerCase().includes(keyword)
+      })
+      res.render('index', { restaurants, keyword })
+    })
+    .catch(error => console.log(error))
+})
+
 router.get('/:way', (req, res) => {
   const way = req.params.way
   Restaurant.find()
